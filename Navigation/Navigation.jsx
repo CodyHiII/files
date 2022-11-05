569a11c4bbc86f28be643ab48c9c2105f52034b8
CodyHiII <51131679+CodyHiII@users.noreply.github.com>
2022-11-05 12:35:48 +0200

    Add files via upload

---
+++ Navigation/Navigation.jsx
@@ -0,0 +1,117 @@
+import { useState, useRef, useEffect, useMemo, useCallback } from "react";
+
+import useMouseMove from "src/hooks/useMouseMove.js";
+import useIsMobile from "src/hooks/useIsMobile.js";
+import useWindowSize from "src/hooks/useWindowSize.js";
+import data from "./data.js";
+
+import styles from "./styles.module.css";
+
+const Navigation = ({ activeNav, setActiveNav }) => {
+  const [mouseOut, setMouseOut] = useState(false);
+  const [isPressedDown, setIsPressedDown] = useState(false);
+  const [touchMoveDistance, setTouchMoveDistance] = useState(0);
+
+  const { mouseX, onMouseMove } = useMouseMove();
+
+  const isMobile = useIsMobile();
+  const isTablet = useIsMobile(992);
+
+  const navContainerRef = useRef();
+  const navTrackRef = useRef();
+
+  const navScrollSpeed = useMemo(() => (isTablet ? 1 : 2), [isTablet]);
+  const navScrollOffset = useMemo(() => (isTablet ? 50 : 200), [isTablet]);
+
+  const touchEvent = touchMoveDistance;
+  const mouseHoverEvent = mouseOut
+    ? -mouseX / navScrollSpeed + navScrollOffset
+    : 0;
+
+  const handleMouseEnter = () => {
+    setMouseOut(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMouseOut(false);
+  };
+
+  const handleNavLinks = () => {
+    setActiveNav(false);
+    console.log("clicked");
+  };
+
+  const handleTouchMove = useCallback((e, offsetX, cursorXPoss) => {
+    if (isPressedDown) return;
+    e.preventDefault();
+    setTouchMoveDistance(offsetX - cursorXPoss);
+  });
+
+  const handleTouchEnd = useCallback(() => {
+    setIsPressedDown(false);
+    console.log("ended");
+  });
+
+  const handleTouchStart = useCallback((e) => {
+    setIsPressedDown(true);
+    const rect = e.target.getBoundingClientRect();
+    const offsetX = e.touches[0].clientX - window.pageXOffset - rect.left;
+    const offsetLeft = navTrackRef.current.scrollLeft;
+    const cursorXPoss = offsetX - offsetLeft;
+
+    navContainerRef.current.addEventListener(
+      "touchmove",
+      handleTouchMove(e, offsetX, cursorXPoss)
+    );
+
+    navContainerRef.current.addEventListener("touchend", handleTouchEnd);
+  });
+
+  useEffect(() => {
+    navContainerRef.current.addEventListener("touchstart", handleTouchStart);
+  }, [isPressedDown]);
+
+  useEffect(() => {
+    navContainerRef.current.addEventListener("mousemove", onMouseMove);
+    return () =>
+      navContainerRef.current.removeEventListener("mousemove", onMouseMove);
+  }, [onMouseMove]);
+
+  return (
+    <nav className={styles.nav}>
+      <div
+        className={`${styles.navContainer} ${
+          activeNav && styles.navContainerActive
+        }`}
+        ref={navContainerRef}
+        onMouseLeave={() => handleMouseLeave()}
+        onMouseEnter={() => handleMouseEnter()}
+      >
+        <div
+          style={{
+            left: `${isPressedDown ? touchEvent : mouseHoverEvent}px`,
+          }}
+          className={styles.navTrack}
+          ref={navTrackRef}
+        >
+          {data.map((link, index) => (
+            <div
+              key={`${link.title} ${index}`}
+              className={styles.navLink}
+              onClick={() => handleNavLinks()}
+            >
+              <a href={link.path}>
+                <div className={styles.linkCardContainer}>
+                  <h4 className={styles.linkCardTitle}>{link.title}</h4>
+                  <div className={styles.linkCardImg}></div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
---
+++ Navigation/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    title: "Home",
+    path: "/home",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Experience",
+    path: "/experience",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+export default data;
